f4c5e2d46ec0ca566acd9046a0a787ebec98d0eb
paco <paco-enriquez@proton.me>
2024-06-01 15:53:53 +0200

    add sdk

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {SDKProvider} from "@tma.js/sdk-react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <SDKProvider acceptCustomStyles debug>
+          <App/>
+      </SDKProvider>
   </React.StrictMode>,
 )
